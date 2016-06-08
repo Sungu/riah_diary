781d419b23daa18cda38345d67ba41141997982c
Sungu Nam <naisz@naver.com>
2016-06-08 09:46:48 +0000

    change in redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,29 @@ module ApplicationHelper
     "Home"
   end
   
-  def markdown(content)
-    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true)
-    @markdown.render(content)
-  end  
+  # def markdown(content)
+  #   @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, fenced_code_blocks: true, hard_wrap: true,space_after_headers: true)
+  #   @markdown.render(content)
+  # end
+  
+  def markdown(text)
+    options = {
+      filter_html:     true,
+      hard_wrap:       true, 
+      link_attributes: { rel: 'nofollow', target: "_blank" },
+      space_after_headers: true, 
+      fenced_code_blocks: true
+    }
+
+    extensions = {
+      autolink:           true,
+      superscript:        true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
