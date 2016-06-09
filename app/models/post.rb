4b3dd07df10c4910bbb669aa40c4bd84552c3dd0
Sungu Nam <naisz@naver.com>
2016-06-09 09:52:23 +0000

    comment push

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  has_many :comments, dependent: :destroy
   # scope :is_private, -> { where(is_private: true) }
   scope :is_public, -> { where(is_private: false) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+  end
+
   match ":controller(/:action(/:id))", :via => [:post, :get]
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
